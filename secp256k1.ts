db67a90ba75ba8a9feb82d751f5826d70cb10d9b
dengjiong <vae520283995@qq.com>
2020-10-01 12:18:22 +0800

    add JacobianPoint

--- secp256k1.ts
+++ secp256k1.ts
@@ -20,6 +20,9 @@ const CURVE = {
     beta: 0x7ae96a2b657c07106e64479eac3434e99cf0497512f58995c1396c28719501een,
 };
 
+let precomputes: Point[];
+let precomputesJ: JacobianPoint[];
+
 class Point {
     static ZERO = new Point(0n, 0n);
 
@@ -57,6 +60,137 @@ class Point {
             n >>= 1n;
         }
         return p;
+    }
+
+    getPrecomputes(): Point[] {
+        if (precomputes) return precomputes;
+        precomputes = [];
+        let dbl: Point = this;
+        for (let i = 0; i < 256; i++) {
+            precomputes.push(dbl);
+            dbl = dbl.double();
+        }
+        return precomputes;
+    }
+
+    multiplyCT(n: bigint) {
+        let dbl = new Point(this.x, this.y);
+        let p = Point.ZERO;
+        let f = Point.ZERO;
+        let dbls = this.getPrecomputes();
+        for (let i = 0; dbl = dbls[i], i < 256; i++) {
+            if ((n & 1n) === 1n) p = p.add(dbl);
+            else f = f.add(dbl);
+            n >>= 1n;
+        }
+        return p;
+    }
+
+    getPrecomputesJ(): JacobianPoint[] {
+        if (precomputesJ) return precomputesJ;
+        precomputesJ = [];
+        let dbl: JacobianPoint = JacobianPoint.fromAffine(this);
+        for (let i = 0; i < 256; i++) {
+            precomputesJ.push(dbl);
+            dbl = dbl.double();
+        }
+        return precomputesJ;
+    }
+
+    multiplyPreCTJ(n: bigint) {
+        const precomputesJ = this.getPrecomputesJ();
+        let dbl = JacobianPoint.fromAffine(this);
+        let p = JacobianPoint.ZERO;
+        let f = JacobianPoint.ZERO;
+        for (let i = 0; dbl = precomputesJ[i], i < 256; i++) {
+            if ((n & 1n)  === 1n) p = p.add(dbl);
+            else f = f.add(dbl);
+            n >>= 1n;
+        }
+        return p.toAffine();
+    }
+}
+
+class JacobianPoint {
+    constructor(public x: bigint, public y: bigint, public z: bigint) {}
+
+    static BASE = new JacobianPoint(CURVE.Gx, CURVE.Gy, 1n);
+    static ZERO = new JacobianPoint(0n, 1n, 0n);
+
+    static fromAffine(p: Point): JacobianPoint {
+        if (!(p instanceof Point)) {
+            throw new TypeError('JacobianPoint#fromAffine: expected Point');
+        }
+        return new JacobianPoint(p.x, p.y, 1n);
+    }
+
+    toAffine(invZ: bigint = invert(this.z)): Point {
+        const invZ2 = invZ ** 2n;
+        const x = mod(this.x * invZ2);
+        const y = mod(this.y * invZ2 * invZ);
+        return new Point(x, y);
+    }
+
+    // Fast algo for doubling 2 Jacobian Points when curve's a=0.
+    // Note: cannot be reused for other curves when a != 0.
+    // From: http://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l
+    // Cost: 2M + 5S + 6add + 3*2 + 1*3 + 1*8.
+    double(): JacobianPoint {
+        const X1 = this.x;
+        const Y1 = this.y;
+        const Z1 = this.z;
+        const A = X1 ** 2n;
+        const B = Y1 ** 2n;
+        const C = B ** 2n;
+        const D = 2n * ((X1 + B) ** 2n - A - C);
+        const E = 3n * A;
+        const F = E ** 2n;
+        const X3 = mod(F - 2n * D);
+        const Y3 = mod(E * (D - X3) - 8n * C);
+        const Z3 = mod(2n * Y1 * Z1);
+        return new JacobianPoint(X3, Y3, Z3);
+    }
+
+    // Fast algo for adding 2 Jacobian Points when curve's a=0.
+    // Note: cannot be reused for other curves when a != 0.
+    // http://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-1998-cmo-2
+    // Cost: 12M + 4S + 6add + 1*2.
+    // Note: 2007 Bernstein-Lange (11M + 5S + 9add + 4*2) is actually *slower*. No idea why.
+    add(other: JacobianPoint): JacobianPoint {
+        if (!(other instanceof JacobianPoint)) {
+            throw new TypeError('JacobianPoint#add: expected JacobianPoint');
+        }
+        const X1 = this.x;
+        const Y1 = this.y;
+        const Z1 = this.z;
+        const X2 = other.x;
+        const Y2 = other.y;
+        const Z2 = other.z;
+        if (X2 === 0n || Y2 === 0n) return this;
+        if (X1 === 0n || Y1 === 0n) return other;
+        const Z1Z1 = Z1 ** 2n;
+        const Z2Z2 = Z2 ** 2n;
+        const U1 = X1 * Z2Z2;
+        const U2 = X2 * Z1Z1;
+        const S1 = Y1 * Z2 * Z2Z2;
+        const S2 = Y2 * Z1 * Z1Z1;
+        const H = mod(U2 - U1);
+        const r = mod(S2 - S1);
+        // H = 0 meaning it's the same point.
+        if (H === 0n) {
+            if (r === 0n) {
+                return this.double();
+            } else {
+                return JacobianPoint.ZERO;
+            }
+        }
+        const HH = mod(H ** 2n);
+        const HHH = mod(H * HH);
+        const V = U1 * HH;
+        const X3 = mod(r ** 2n - HHH - 2n * V);
+        const Y3 = mod(r * (V - X3) - S1 * HHH);
+        const Z3 = mod(Z1 * Z2 * H);
+        return new JacobianPoint(X3, Y3, Z3);
     }
 }
 
@@ -99,4 +233,44 @@ function getPublicKey(privKey: bigint) {
     return G.multiplyDA(privKey);
 }
 
-console.log(getPublicKey(64165165165165165161561n));
+console.log(getPublicKey(2n ** 255n - 19n));
+console.log(G.multiplyPreCTJ(2n ** 255n - 19n));
+
+const {run, mark, logMem} = require('micro-bmark');
+
+// run([4, 8, 16], async (windowSize) => {
+run(async () => {
+    const samples = 1;
+    //console.log(`-------\nBenchmarking window=${windowSize} samples=${samples}...`);
+
+    logMem();
+    console.log();
+
+    // await mark('getPublicKey 256 bit', samples, () => {
+    //     G.multiplyCT(2n ** 255n - 42n);
+    // });
+    //
+    // await mark('getPublicKey 1 bit', samples * 10, () => {
+    //     G.multiplyCT(2n);
+    // });
+    //
+    // await mark('getPublicKey 256 bit', samples * 10, () => {
+    //     G.multiplyCT(2n ** 255n - 42n);
+    // });
+
+    await mark('multiplyPreCTJ 256 bit', samples, () => {
+        G.multiplyPreCTJ(2n ** 255n - 42n);
+    });
+
+    await mark('multiplyPreCTJ 1 bit', samples * 10, () => {
+        G.multiplyPreCTJ(2n);
+    });
+
+    await mark('multiplyPreCTJ 256 bit', samples * 10, () => {
+        G.multiplyPreCTJ(2n ** 255n - 42n);
+    });
+
+    console.log();
+    logMem();
+});
+
